abc6f325f05c851684be2875e131f5158b051348
okioka <komiya417@gmail.com>
2020-04-08 22:42:30 +0900

    fix layout

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,8 +1,13 @@
 import App  from '../components/App.js'
+import Head from 'next/head'
 import css from '../public/styles.scss'
 
 const Index = () => (
   <div>
+    <Head>
+      <title>アニメ検索</title>
+      <meta name="viewport" content="width=1280"></meta>
+    </Head>
     <App />
   </div>
 )
